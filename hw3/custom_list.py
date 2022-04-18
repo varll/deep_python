b5ebabd51c26875ae6ce5963cfee8934f81c7af7
varll <93476866+varll@users.noreply.github.com>
2022-04-18 22:20:27 +0300

    Add files via upload

---
+++ hw3/custom_list.py
@@ -0,0 +1,38 @@
+class CustomList(list):
+
+    def __add__(self, other):
+        new_len = max(len(self), len(other))
+        new_list = CustomList([0]*new_len)
+
+        for i, val in enumerate(self):
+            new_list[i] += val
+        for i, val in enumerate(other):
+            new_list[i] += val
+
+        return new_list
+
+    __radd__ = __add__
+
+    def __neg__(self):
+        return CustomList(-i for i in self)
+
+    def __sub__(self, other):
+        return -(-self + other)
+
+    def __rsub__(self, other):
+        return -self + other
+
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    def __lt__(self, other):
+        return sum(self) < sum(other)
+
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __ne__(self, other):
+        return sum(self) != sum(other)
+
+    def __str__(self):
+        return f'{list(self)}, {sum(self)}'
---
+++ hw3/tests.py
@@ -0,0 +1,72 @@
+import unittest
+from custom_list import CustomList
+
+
+class TestCustomList(unittest.TestCase):
+    def setUp(self):
+        self.test_list1 = CustomList([1, 2, 3])
+        self.test_list2 = CustomList([3, 4])
+        self.test_empty_list = CustomList()
+
+    def test_add(self):
+        self.assertEqual(self.test_list1 + self.test_list2, [4, 6, 3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+        self.assertEqual(self.test_list2, [3, 4])
+
+        self.assertEqual(self.test_list1 + [1], [2, 2, 3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+
+        self.assertEqual([1, 2] + self.test_list1, [2, 4, 3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+
+        self.assertEqual(self.test_list1 + [], [1, 2, 3])
+        self.assertEqual(self.test_list1 + self.test_empty_list, [1, 2, 3])
+
+    def test_sub(self):
+        self.assertEqual(self.test_list1 - self.test_list2, [-2, -2, 3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+        self.assertEqual(self.test_list2, [3, 4])
+
+        self.assertEqual(self.test_list1 - [1], [0, 2, 3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+
+        self.assertEqual([1, 2] - self.test_list1, [0, 0, -3])
+        self.assertEqual(self.test_list1, [1, 2, 3])
+
+        self.assertEqual(self.test_list1 - [], [1, 2, 3])
+        self.assertEqual(self.test_list1 - self.test_empty_list, [1, 2, 3])
+
+    def test_type(self):
+        self.assertEqual(type(self.test_list1 + self.test_list2), CustomList)
+        self.assertEqual(type(self.test_list1 + []), CustomList)
+        self.assertEqual(type([1, 3, 8] + self.test_list1), CustomList)
+
+        self.assertEqual(type(self.test_list1 - self.test_list2), CustomList)
+        self.assertEqual(type(self.test_list1 - []), CustomList)
+        self.assertEqual(type([1, 3, 8] - self.test_list1), CustomList)
+
+    def test_eq(self):
+        self.assertTrue(self.test_list1 == self.test_list1)
+        self.assertTrue(self.test_list1 == [1, 2, 3])
+        self.assertTrue([1, 2, 3] == self.test_list1)
+        self.assertTrue(self.test_empty_list == [])
+
+    def test_ne(self):
+        self.assertTrue(self.test_list1 != self.test_list2)
+        self.assertTrue(self.test_list1 != [5, 5, 5])
+        self.assertTrue([] != self.test_list1)
+        self.assertTrue(self.test_empty_list != [1])
+
+    def test_gt(self):
+        self.assertTrue(self.test_list2 > self.test_empty_list)
+        self.assertTrue(self.test_list1 > [])
+        self.assertTrue([3, 3, 3, 3, 3] > self.test_list1)
+
+    def test_lt(self):
+        self.assertTrue(self.test_list1 < self.test_list2)
+        self.assertTrue([] < self.test_list1)
+        self.assertTrue([3] < self.test_list1)
+
+
+if __name__ == "__main__":
+    unittest.main()

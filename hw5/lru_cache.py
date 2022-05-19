f755a9ab10e79cc50cf7abd4cefcbeee689bec45
varll <93476866+varll@users.noreply.github.com>
2022-05-19 20:20:46 +0300

    Update lru_cache.py

--- hw5/lru_cache.py
+++ hw5/lru_cache.py
@@ -36,20 +36,3 @@ class LRUCache:
 
     def __str__(self):
         return f'{self.lru_cache}'
-
-
-if __name__ == '__main__':
-    cache = LRUCache(2)
-
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
-
-    print(cache.get("k3"))  # None
-    print(cache.get("k2"))  # "val2"
-    print(cache.get("k1"))  # "val1"
-
-    cache.set("k3", "val3")
-
-    print(cache.get("k3"))  # "val3"
-    print(cache.get("k2"))  # None
-    print(cache.get("k1"))  # "val1"

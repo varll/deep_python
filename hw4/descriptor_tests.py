e618036636467c54c70279edc97e00e0ed7ab4bf
varll <93476866+varll@users.noreply.github.com>
2022-05-19 22:25:14 +0300

    Add files via upload

--- hw4/descriptor_tests.py
+++ hw4/descriptor_tests.py
@@ -1,13 +1,14 @@
 import unittest
-from descriptor import Data
+from descriptor import Data, Integer, String, PositiveInteger
 
 
 class TestDescriptor(unittest.TestCase):
     def test_creation(self):
-        test_data = Data(-8, 'test', 22)
-        self.assertEqual(test_data.num, -8)
+        test_data = Data(0, 'test', 0)
+
+        self.assertEqual(test_data.num, 0)
         self.assertEqual(test_data.name, 'test')
-        self.assertEqual(test_data.price, 22)
+        self.assertEqual(test_data.price, 0)
 
     def test_num(self):
         with self.assertRaises(TypeError):
@@ -30,17 +31,23 @@ class TestDescriptor(unittest.TestCase):
     def test_change_val(self):
         test_data = Data(0, 'test', 0)
 
+        test_data.num = 4
+        self.assertEqual(test_data.num, 4)
         with self.assertRaises(TypeError):
             test_data.num = 'test'
-        self.assertEqual(test_data.num, 0)
+        self.assertEqual(test_data.num, 4)
 
+        test_data.name = 'test'
+        self.assertEqual(test_data.name, 'test')
         with self.assertRaises(TypeError):
             test_data.name = -1
         self.assertEqual(test_data.name, 'test')
 
+        test_data.price = 25
+        self.assertEqual(test_data.price, 25)
         with self.assertRaises(ValueError):
             test_data.price = -1
-        self.assertEqual(test_data.price, 0)
+        self.assertEqual(test_data.price, 25)
 
 
 if __name__ == '__main__':
